d4624b06e4eb82e01c4385941ce884e201bb2695
illuminate3 <carsten.sapporo@gmail.com>
2015-09-30 10:46:05 -0500

    dev 3.5.1

--- Resources/Views/news/create.blade.php
+++ Resources/Views/news/create.blade.php
@@ -87,14 +87,14 @@ function setImage(select){
 
 	@foreach( $languages as $language)
 	<div role="tabpanel" class="tab-pane padding fade @if ($language->locale == $lang)in active @endif" id="lang_{{{ $language->id }}}">
-			<div class="form-group">
-				<label for="content">{{ trans('kotoba::cms.content') }}</label>
-				<textarea class="form-control summernote" name="{{ 'content_'. $language->id }}" id="{{ 'content_'. $language->id }}" placeholder="{{ trans('kotoba::cms.content') }}"></textarea>
-			</div>
 
 			<div class="form-group">
 				<label for="title">{{ trans('kotoba::general.title') }}</label>
 				<input type="text" class="form-control" name="{{ 'title_'. $language->id }}" id="{{ 'title_'. $language->id }}" placeholder="{{ trans('kotoba::general.title') }}">
+			</div>
+			<div class="form-group">
+				<label for="content">{{ trans('kotoba::cms.content') }}</label>
+				<textarea class="form-control summernote" name="{{ 'content_'. $language->id }}" id="{{ 'content_'. $language->id }}" placeholder="{{ trans('kotoba::cms.content') }}"></textarea>
 			</div>
 
 			<div class="form-group">
